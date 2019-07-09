bfc08a70f68a7463ec29eb0bbc25084ad57b3973
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2019-07-09 13:49:36 -0700

    No need for session variable for this project

--- server_mysql.py
+++ server_mysql.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect
 from mysqlconnection import connectToMySQL
 app = Flask(__name__)
 
