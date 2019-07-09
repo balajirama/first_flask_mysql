5f45b10e11862d9ad5bac717e635272cde60c96e
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2019-07-09 16:49:21 -0700

    Don't post the sqlite3 database
    Every change will require a Git commit

--- server_sqlite3.py
+++ server_sqlite3.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request, redirect
 from sqlite3connection import connectToSQLite3
 app = Flask(__name__)
 
+dbname = '/home/balaji/my_db.db'
 
 @app.route("/")
 def index():
-    conn = connectToSQLite3('my_db.db')
+    conn = connectToSQLite3(dbname)
     all_animals = conn.query_db('SELECT * FROM animals ORDER BY common_name')
     all_pets = conn.query_db('SELECT pets.id, name, common_name FROM pets JOIN animals ON pets.animal_id = animals.id ;')
     return render_template("index2.html", all_animals=all_animals, all_pets=all_pets)
@@ -13,7 +14,7 @@ def index():
 @app.route("/add_pet", methods=["POST"])
 def add_pet():
     print(request.form)
-    conn = connectToSQLite3('my_db.db')
+    conn = connectToSQLite3(dbname)
     data = {
         'name': request.form['pet_name'],
         'id': int(request.form['pet_type'])
